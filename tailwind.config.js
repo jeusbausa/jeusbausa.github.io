11a89e6bfb8a99bec064986a42b972642c7b8e15
jeiyuseu@gmail.com <jeiyuseu@gmail.com>
2021-11-14 19:03:52 +0800

    improved new design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,41 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		screens: {
+			xss: '320px',
+			xs: '475px',
+			...defaultTheme.screens,
+		},
 		fontFamily: {
 			poppins: ['Poppins'],
+			openSans: ['Open Sans'],
 		},
-		extend: {},
+		extend: {
+			width: {
+				portfolioWidth: '450px',
+			},
+			colors: {
+				mainBg: '#121212',
+				primaryBg: '#9DEDF8',
+				secondaryBg: '#9DEDF8',
+				pBg: '#A1AEC1',
+				bgCard: '#252525',
+			},
+		},
 	},
 	variants: {
-		extend: {},
+		extend: {
+			transitionTimingFunction: ['hover', 'focus'],
+			transitionDuration: ['hover', 'focus'],
+			textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+			borderWidth: ['hover'],
+			transform: ['hover'],
+			scale: ['hover'],
+			fontSize: ['hover', 'focus'],
+			transitionProperty: ['hover', 'focus'],
+		},
 	},
 	plugins: [],
 }
