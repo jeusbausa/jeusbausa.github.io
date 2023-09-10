201b204a43f3e23a8c0c989b6abf5c8ac007c9d5
Jeus <jeusdbausa@gmail.com>
2023-09-11 03:38:02 +0800

    updated to react-vite

--- public/js/toggle.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector('#toggle')
-const navbar = document.querySelector('#navbar')
-const link = document.querySelector('#links')
-toggle.addEventListener('click', () => {
-	navbar.classList.toggle('hidden')
-	link.addEventListener('click', () => navbar.classList.add('hidden'))
-})
