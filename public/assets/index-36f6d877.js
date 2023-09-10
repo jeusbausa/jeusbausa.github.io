0b3e9decc175d2e0e4400ca0db2b939c2b42354d
Jeus <jeusdbausa@gmail.com>
2023-09-11 03:42:28 +0800

    updated build files

--- public/assets/index-36f6d877.js
+++ public/assets/index-36f6d877.js
@@ -1,4 +1,4 @@
-import{g as l}from"./index-e81f58d5.js";var c={exports:{}};/*!
+import{g as l}from"./index-135ecaeb.js";var c={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
