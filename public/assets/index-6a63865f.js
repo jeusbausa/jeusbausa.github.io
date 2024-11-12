01fed731cde5090bf7f168e96d8fbbe0e589538d
Jeus B. <jeusdbausa@gmail.com>
2024-11-12 11:06:13 +0800

    update avatar src

--- public/assets/index-6a63865f.js
+++ public/assets/index-6a63865f.js
@@ -1,4 +1,4 @@
-import{g as l}from"./index-8a3f15d3.js";var c={exports:{}};/*!
+import{g as l}from"./index-414733f0.js";var c={exports:{}};/*!
 	Copyright (c) 2018 Jed Watson.
 	Licensed under the MIT License (MIT), see
 	http://jedwatson.github.io/classnames
