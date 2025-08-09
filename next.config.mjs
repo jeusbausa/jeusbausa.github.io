813e53f979c601093417bf0b389a6d3da42f3a31
Jeus B. <jeusdbausa@gmail.com>
2025-08-09 23:57:08 +0800

    added fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-
+const repo = "jeusbausa.github.io";
 const nextConfig = {
   reactStrictMode: false,
   images: {
@@ -11,5 +11,7 @@ const nextConfig = {
   },
   output: "export",
   images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 export default nextConfig;
